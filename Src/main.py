9409eedb0a55900f20128b3aab44c6142c6e8fe9
RClayGates <113868689+Ninjasniperc@users.noreply.github.com>
2024-02-15 12:02:17 -0500

    The example code with todos/todones added

---
+++ Src/main.py
@@ -0,0 +1,17 @@
+# import: std
+
+# import: non-std
+
+# import: locals
+
+# constants
+
+# main
+def main():
+    # TODO: Make code that will rock user's socks
+    pass
+
+# code blocks
+
+if __name__ == "__main__":
+    main()
---
+++ Src/subdir/stuff.py
@@ -0,0 +1,23 @@
+# import: std
+
+# import: non-std
+
+# import: locals
+# TODO: Create database wrapper.py
+
+# constants
+
+# main
+def main():
+    pass
+
+# code blocks
+# TODONE: Create Convenience Class
+class Convenience:
+
+    def __init__(self) -> None:
+        pass
+    # TODO: Make getter/setter methods
+
+if __name__ == "__main__":
+    main()
